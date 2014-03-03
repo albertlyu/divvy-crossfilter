a7cac8e91bd55b070f2f757d82590cdeb8e4a99a
Albert Lyu <albertworld@gmail.com>
2014-03-02 22:04:14 -0600

    Added several comparison filters, such as weekdays, summer/winter, mornings/nights, etc.

--- js/format.js
+++ js/format.js
@@ -1,6 +1,5 @@
 // Various formatters.
 var formatNumber = d3.format(",d"),
-	formatChange = d3.format("+,d"),
 	formatDate = d3.time.format("%B %d, %Y"),
 	formatTime = d3.time.format("%I:%M %p");
 
@@ -24,4 +23,35 @@ function formatDate(number) {
 	format = d3.time.format("%m/%d/%y");
 	newNumber = format(number);
 	return newNumber
+}
+
+function getDayOfWeek(time) {
+	var weekday = new Array();
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+	dayOfWeek = weekday[time.getDay()];
+	return dayOfWeek
+}
+
+function getMonthOfYear(time) {
+	var month = new Array();
+	month[0] = "January";
+	month[1] = "February";
+	month[2] = "March";
+	month[3] = "April";
+	month[4] = "May";
+	month[5] = "June";
+	month[6] = "July";
+	month[7] = "August";
+	month[8] = "September";
+	month[9] = "October";
+	month[10] = "November";
+	month[11] = "December";
+	monthOfYear = month[time.getMonth()];
+	return monthOfYear
 }
